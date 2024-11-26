f1939ad88d43961900314e3709ea86d4ca024452
Ledion Saiti <lsaiti@k1r1p14.42mulhouse.fr>
2024-11-26 17:22:59 +0100

    fdf ff

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ SRC = main.c get_next_line.c get_next_line_utils.c ft_atoi.c ft_split.c
 INCLUDES = ./includes
 CC = cc
 
-FLAGS = -Wall -Werror -Wextra -lX11 -lXext -lmlx -g
+FLAGS = -Wall -Werror -Wextra -lX11 -lXext -lmlx -g -lm
 
 NAME = fdf
 
--- main.c
+++ main.c
@@ -6,12 +6,12 @@
 /*   By: lsaiti <lsaiti@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 15:40:46 by lsaiti            #+#    #+#             */
-/*   Updated: 2024/11/21 19:15:34 by lsaiti           ###   ########.fr       */
+/*   Updated: 2024/11/26 17:12:08 by lsaiti           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/fdf.h"
-
+#include <math.h>
 
 #define HAUTEUR 1920
 
@@ -67,19 +67,86 @@ int	**map_parsing(char *file_name)
 	return (map);
 }
 
+// int get_x(int x, int y)
+// {
+//     // Projette x et y en isométrique
+//     double iso_x = (sqrt(2.0) / 2.0) * (x - y);
+//     // Centre l'axe X sur l'écran
+//     return (int)((iso_x * (HAUTEUR / 2)) / 19.0 + (HAUTEUR / 2));
+// }
+
+// int get_y(int x, int y, int z)
+// {
+//     // Projette x, y et z en isométrique
+//     double factor1 = sqrt(2.0 / 3.0);
+//     double factor2 = 1.0 / sqrt(6.0);
+//     double iso_y = (factor1 * z) - (factor2 * (x + y));
+//     // Centre l'axe Y sur l'écran
+//     return (int)((iso_y * (LARGEUR / 2)) / 11.0 + (LARGEUR / 2));
+// }
+
+// int get_x(int x, int y)
+// {
+//     // Projette x et y en isométrique
+//     double iso_x = (sqrt(2.0) / 2.0) * (x - y);
+//     // Centre l'axe X sur l'écran
+//     return (abs((int)iso_x));
+// }
+
+// int get_y(int x, int y, int z)
+// {
+//     // Projette x, y et z en isométrique
+//     double factor1 = sqrt(2.0 / 3.0);
+//     double factor2 = 1.0 / sqrt(6.0);
+//     double iso_y = (factor1 * z) - (factor2 * (x + y));
+//     // Centre l'axe Y sur l'écran
+//     return (abs((int)iso_y));
+// }
+
+int get_x(int x, int y)
+{
+	double	i;
+
+	i = ((sqrt(2) / 2) * (x - y));
+	i *= -1;
+	i = (i * (HAUTEUR / 2))/ 19;
+	i += HAUTEUR / 2;
+	return (int)(i);
+}
+
+int	get_y(int x, int y, int z)
+{
+	double	i;
+	
+	i = ((sqrt(2 / 3) * z) - ((1 / sqrt(6))* (x + y)));
+	i *= -1;
+	i = (i * (LARGEUR / 2)) / 11;
+	i += LARGEUR / 2;
+	return (int)(i);
+}
+
 void	draw_map(void *mlx, void *wint_ptr, int **map)
 {
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
+
+	i = 0;
 	while (i < 11)
 	{
 		j = 0;
 		while (j < 19)
 		{
 			if (map[i][j] == 0)
-				mlx_pixel_put(mlx, wint_ptr, (j * HAUTEUR) / 19, (i * LARGEUR) / 11, 0xFFFFFF);
+			{
+				printf("%d, %d\n", get_x(i - 10, j - 5), get_y(i - 10, j - 5, 0));
+				mlx_pixel_put(mlx, wint_ptr, get_x(i - 10, j - 5), get_y(i - 10,j - 5, 0), 0xFFFFFF);
+			}
 			else
-				mlx_pixel_put(mlx, wint_ptr, (j * HAUTEUR) / 19, (i * LARGEUR) / 11, 0xFF0000);
+			{
+				printf("??%d, %d??\n", get_x(i - 10, j - 5), get_y(i - 10, j - 5, map[i][j]));
+				printf("??%d, %d??\n", get_x(i - 10, j - 5), get_y(i - 10, j - 5, 0));
+				mlx_pixel_put(mlx, wint_ptr, get_x(i - 10, j - 5) , get_y(i - 10, j - 5, map[i][j]), 0xFF0000);
+			}
 			j++;
 		}
 		i++;
@@ -102,3 +169,4 @@ int main(void)
 		
 	}
 }
+// (HAUTEUR / 2)* (1 + ( ((j * f) / 10) / (19)))
